b1bd500e145229cc356281405f3c848552c13267
Tim Quimbo <tquimbo1@gmail.com>
2021-03-02 04:00:48 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
-
+return speed > 60 || speed < 40
 end
 
 
-
+#
 def not_safe?(speed)
-	
+speed > 60 || speed < 40 ? true : false
 end
-	
 
-
+#
+# age = 1
+#
+# age < 2 ? "baby" : "not a baby"

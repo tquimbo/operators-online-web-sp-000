b558cc4655d91889068704627c2ded5dbdb3e8eb
Tim Quimbo <tquimbo1@gmail.com>
2021-03-02 03:45:19 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,16 +1,16 @@
 def unsafe?(speed)
 speed = 50
-if 40 > speed > 60 ? return true : return false
+40 > speed > 60 ? return true : return false
 
 end
 end
 
 
 
-def not_safe?(speed)
-
-end
-
+# def not_safe?(speed)
+#
+# end
+#
 
 #
 # age = 1
